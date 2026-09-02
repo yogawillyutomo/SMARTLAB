a1a2043a654240d82f4625eaaee70acd8b86f292
Yoga Willy <yogawilly@outlook.com>
2026-09-03 03:06:20 +0700

    test(api): align incident route registry with comments

--- apps/api/tests/Feature/IncidentReportingContextApiTest.php
+++ apps/api/tests/Feature/IncidentReportingContextApiTest.php
@@ -247,6 +247,8 @@ class IncidentReportingContextApiTest extends TestCase
             'api/v1/incidents/{incidentId}',
             'api/v1/incidents/{incidentId}/assignments',
             'api/v1/incidents/{incidentId}/transitions',
+            'api/v1/incidents/{incidentId}/comments',
+            'api/v1/incidents/{incidentId}/comments',
         ], $routes->map(fn ($route): string => $route->uri())->all());
         $this->assertContains('permission:incidents.create', $routes[0]->gatherMiddleware());
         $this->assertContains('permission:incidents.create', $routes[1]->gatherMiddleware());
@@ -264,6 +266,12 @@ class IncidentReportingContextApiTest extends TestCase
         $this->assertNotContains('permission:incidents.approve', $routes[9]->gatherMiddleware());
         $this->assertNotContains('permission:incidents.update', $routes[9]->gatherMiddleware());
         $this->assertNotContains('permission:incidents.assign', $routes[9]->gatherMiddleware());
+        $this->assertContains('permission:incidents.view', $routes[10]->gatherMiddleware());
+        $this->assertNotContains('permission:incidents.comment', $routes[10]->gatherMiddleware());
+        $this->assertNotContains('permission:incidents.view-history', $routes[10]->gatherMiddleware());
+        $this->assertContains('permission:incidents.view', $routes[11]->gatherMiddleware());
+        $this->assertContains('permission:incidents.comment', $routes[11]->gatherMiddleware());
+        $this->assertNotContains('permission:incidents.view-history', $routes[11]->gatherMiddleware());
         $this->assertNotContains('permission:incidents.view', $routes[2]->gatherMiddleware());
         $this->assertNotContains('permission:laboratories.view', $routes[0]->gatherMiddleware());
         $this->assertNotContains('permission:devices.view', $routes[1]->gatherMiddleware());
