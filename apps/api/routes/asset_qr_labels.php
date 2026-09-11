00b05e1728ee8d74a97c7326a20fe0b1971e5019
Yoga Willy <yogawilly@outlook.com>
2026-09-11 07:24:58 +0700

    fix(s5.6): allow label generation with existing QR

--- apps/api/routes/asset_qr_labels.php
+++ apps/api/routes/asset_qr_labels.php
@@ -13,7 +13,7 @@ Route::prefix('api/v1')
             Route::get('asset-qr-label-batches', [AssetQrLabelBatchController::class, 'index'])
                 ->middleware('permission:assets.generate-labels');
             Route::post('asset-qr-label-batches', [AssetQrLabelBatchController::class, 'store'])
-                ->middleware(['permission:assets.generate-labels', 'permission:assets.manage-qr', 'permission:assets.view']);
+                ->middleware(['permission:assets.generate-labels', 'permission:assets.view']);
             Route::get('asset-qr-label-batches/{batchId}', [AssetQrLabelBatchController::class, 'show'])
                 ->middleware('permission:assets.generate-labels');
             Route::post('asset-qr-label-batches/{batchId}/reprint', [AssetQrLabelBatchController::class, 'reprint'])
